4f64653be1b4dc9708583d35a7d07ff82e7c501a
Danny016 <danydango001@gmail.com>
2026-01-17 09:51:35 -0600

    Add basic examples with Options API and Composition API

---
+++ src/Contador.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+    // console.log('Holita');
+    export default {
+        data() {
+            return {
+                count: 0
+            };
+        },
+        methods: {
+            increse() {
+                this.count++;
+            },
+            decrese() {
+                this.count--;
+            }
+        },
+        mounted() {
+            console.log('Componente Contador montado '+ this.count)
+        }
+    }
+</script>
+
+<template>
+    <h1>Contador: {{ count }}</h1>
+    <button @click="increse">Incrementar</button>
+    <button @click="decrese">Decrementar</button>
+</template>
---
+++ src/examples/ContadorComposition.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    import {onMounted, ref} from 'vue';
+    const contador =ref(0);
+    function incrementar(){
+        contador.value++;
+    }
+    onMounted(function() {
+        console.log('El valor del contador es: '+ contador.value);
+    });
+</script>
+
+<template>
+    <p>hla soy la version con Composition API</p>
+    <h1>Contador: {{ contador }}</h1>
+    <button @click="incrementar">Incrementar</button>
+</template>
